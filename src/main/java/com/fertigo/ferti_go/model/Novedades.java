e600a2d34a68b2d10c3e9075b880f1d6f02bf09a
bratt10 <bratt565699@gmail.com>
2025-12-10 10:41:40 -0500

    correccion en novedades

--- src/main/java/com/fertigo/ferti_go/model/Novedades.java
+++ src/main/java/com/fertigo/ferti_go/model/Novedades.java
@@ -1,24 +1,31 @@
 package com.fertigo.ferti_go.model;
+
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "novedades")
 @Data
 public class Novedades {
-    @Id @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idNovedad;
     @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
-    @NotBlank(message = "El nombre es obligatorio")
+    @NotBlank(message = "El nombre de la finca es obligatorio")
     private String nombreDeFinca;
     @Email(message = "Correo no válido")
     private String correo;
     @NotBlank(message = "El mensaje es obligatorio")
     private String novedad;
+    private LocalDate fechaEnvio;
+    @PrePersist
+    public void asignarFecha() {
+        this.fechaEnvio = LocalDate.now();
+    }
 }
